2e0f8c8c89edb52819ab15827efc1dbcc3880994
740133670@qq.com <Administrator@windows10.microdone.cn>
2020-03-07 17:53:51 +0800

    飞机大战

---
+++ vip2002/src/day02/demo1.java
@@ -0,0 +1,32 @@
+package day02;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		System.out.println(325/10%10);
+//		//运算符  %  ++  --  &&  ||
+		int a = 3,b= 4,c;
+		c = a++ +2;
+//		5;
+		c = 2;
+//		// a  b  c   a>b?a>c?a:b
+//		boolean s = a>b?true:a>c?true:false;
+//		
+//		 boolean b3=false?false:true==true?false:true;
+		if(a==c)
+			System.out.println("111");
+		System.out.println(22);
+		byte b1 = 10, b2 = 20;
+		  byte b11=1+2;
+
+	
+		//  编写程序求1到10的和并输出该结果?
+		  
+		  // i=   1 + 2+3+4...10
+		  /*
+		   * int num = i+1
+		   * 1   1+1    2+1  
+		   */
+	}
+
+}
